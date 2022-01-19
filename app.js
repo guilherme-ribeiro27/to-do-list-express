97eab501706376379d1e33e9d7aed23b9e814cce
guilherme-ribeiro27 <gui062702@gmail.com>
2022-01-19 09:00:32 -0300

    First Router Insertion

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const checklistsRouter = require('./src/routes/checklist')
+
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('<h1>Minha lista de tarefas:)</h1>')
-})
+app.use(express.json())
+app.use('/checklists',checklistsRouter)
+
 
 app.listen(3000,()=>{
     console.log(`Servidor foi iniciado na porta http://localhost:3000`)

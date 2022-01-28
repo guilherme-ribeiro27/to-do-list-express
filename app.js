d9b7b5828a50c104dcac8172fb494e151ee9ad29
guilherme-ribeiro27 <gui062702@gmail.com>
2022-01-28 08:17:56 -0300

    New settings (routes,view engine...)

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require('path')
+
 const checklistsRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
 require('./config/database') //sem variável o node roda o arquivo
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public'))) //todos os arquivos estáticos vão fica na pasta public
+
+app.set('views',path.join(__dirname,'src/views')) // todas as views vão ficar na pasta views
+app.set('view engine','ejs') // setando a engine de views dinâmicas é o EJS
+
+app.use('/',rootRouter)
 app.use('/checklists',checklistsRouter)
 
 

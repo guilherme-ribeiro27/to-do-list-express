f797edf0c2581e6bff14b226482e08d75eb599ee
guilherme-ribeiro27 <gui062702@gmail.com>
2022-01-28 11:28:38 -0300

    Adding methodOverride

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const checklistsRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
+const methodOverride = require('method-override')
 
 require('./config/database') //sem variável o node roda o arquivo
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public'))) //todos os arquivos estáticos vão fica na pasta public
+app.use(methodOverride('_method'))
 
 app.set('views',path.join(__dirname,'src/views')) // todas as views vão ficar na pasta views
 app.set('view engine','ejs') // setando a engine de views dinâmicas é o EJS

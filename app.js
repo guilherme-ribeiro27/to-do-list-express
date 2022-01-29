ef1b64d0239a7e4d5120cae147bb8edab684e80c
guilherme-ribeiro27 <gui062702@gmail.com>
2022-01-29 10:29:59 -0300

    Reseting some settings

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const checklistsRouter = require('./src/routes/checklist')
+const taskRouter = require('./src/routes/tasks')
 const rootRouter = require('./src/routes/index')
 const methodOverride = require('method-override')
 
@@ -9,15 +10,19 @@ require('./config/database') //sem variável o node roda o arquivo
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method',{ methods: ['POST','GET']}))
+
 app.use(express.static(path.join(__dirname,'public'))) //todos os arquivos estáticos vão fica na pasta public
-app.use(methodOverride('_method'))
+
 
 app.set('views',path.join(__dirname,'src/views')) // todas as views vão ficar na pasta views
 app.set('view engine','ejs') // setando a engine de views dinâmicas é o EJS
 
 app.use('/',rootRouter)
 app.use('/checklists',checklistsRouter)
+app.use('/checklists', taskRouter.checklistDependent)
+app.use('/tasks',taskRouter.simple)
 
 
 app.listen(3000,()=>{
